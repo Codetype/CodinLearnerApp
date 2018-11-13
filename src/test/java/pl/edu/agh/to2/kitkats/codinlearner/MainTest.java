e4a297c5632c5c0568ff207e82044a96708ec737
Rafał Cegielski <cegielski.rafaal@gmail.com>
2018-11-13 20:40:01 +0100

    Initial Commit

---
+++ src/test/java/pl/edu/agh/to2/kitkats/codinlearner/MainTest.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to2.kitkats.codinlearner;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+}
