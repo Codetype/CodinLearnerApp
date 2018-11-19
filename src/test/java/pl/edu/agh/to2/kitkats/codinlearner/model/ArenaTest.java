9f8d654b436f5cdfb0326190612e1ce8eedab9ef
Paweł Gędłek <pawelgedlek@gmail.com>
2018-11-20 00:35:37 +0100

    Adding mock tests

--- src/test/java/pl/edu/agh/to2/kitkats/codinlearner/model/ArenaTest.java
+++ src/test/java/pl/edu/agh/to2/kitkats/codinlearner/model/ArenaTest.java
@@ -1,17 +1,24 @@
 package pl.edu.agh.to2.kitkats.codinlearner.model;
 
-;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class ArenaTest {
+    private Arena arena = null;
+
+    @Before
+    public void beforeEachTest(){
+        arena = Mockito.mock(Arena.class);
+        arena = new Arena(600.0f, 400.0f);
+    }
 
     @Test
     public void testArenaWidth() {
-        Arena arena = new Arena(600.0f, 400.0f);
         float w = arena.getWidth();
 
         assertEquals(w, 600,  0.0);
@@ -19,7 +26,6 @@ public class ArenaTest {
 
     @Test
     public void testArenaHeight() {
-        Arena arena = new Arena(600.0f, 400.0f);
         float h = arena.getHeight();
 
         assertEquals(h, 400, 0.0);
@@ -27,12 +33,9 @@ public class ArenaTest {
 
     @Test
     public void testArenaTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-
         Cursor t1 = arena.getCursor();
         ObjectProperty<Cursor> cursor = new SimpleObjectProperty<Cursor>(new Cursor(600.0f, 400.0f, arena, 50.0f));
 
         assertNotEquals(cursor, t1);
     }
-
 }
--- src/test/java/pl/edu/agh/to2/kitkats/codinlearner/model/CursorTest.java
+++ src/test/java/pl/edu/agh/to2/kitkats/codinlearner/model/CursorTest.java
@@ -1,17 +1,25 @@
 package pl.edu.agh.to2.kitkats.codinlearner.model;
-import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 
 public class CursorTest {
+    private Arena arena = null;
+    private Cursor cursor = null;
+
+    @Before
+    public void beforeEachTest(){
+        arena = Mockito.mock(Arena.class);
+        arena = new Arena(600.0f, 400.0f);
+        cursor = Mockito.mock(Cursor.class);
+        cursor = new Cursor(300.0f, 200.0f, arena, 10.0f);
+    }
 
     @Test
-    public void testInitTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 10.0f);
-
+    public void testInitCursor() {
         double x1 = cursor.getX();
         double y1 = cursor.getY();
         assertEquals(x1, 150, 0.0);
@@ -20,11 +28,7 @@ public class CursorTest {
 
 
     @Test
-    public void testResetTurtlePosition() {
-
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 20.0f);
-
+    public void testResetCursorPosition() {
         cursor.move();
         cursor.turnLeft();
         cursor.move();
@@ -40,10 +44,7 @@ public class CursorTest {
 
 
     @Test
-    public void testGoForwardTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 10.0f);
-
+    public void testGoForwardCursor() {
         cursor.move();
 
         double x1 = cursor.getX();
@@ -53,38 +54,29 @@ public class CursorTest {
     }
 
     @Test
-    public void testGoLeftTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 20.0f);
-
+    public void testGoLeftCursor() {
         cursor.turnLeft();
         cursor.move();
 
         double x1 = cursor.getX();
         double y1 = cursor.getY();
         assertEquals(150, x1, 0.0);
-        assertEquals(80, y1, 0.0);
+        assertEquals(90, y1, 0.0);
     }
 
     @Test
-    public void testGoRightTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 30.0f);
-
+    public void testGoRightCursor() {
         cursor.turnRight();
         cursor.move();
 
         double x1 = cursor.getX();
         double y1 = cursor.getY();
         assertEquals(150, x1, 0.0);
-        assertEquals(130, y1, 0.0);
+        assertEquals(110, y1, 0.0);
     }
 
     @Test
-    public void testRotateTurtle() {
-        Arena arena = new Arena(600.0f, 400.0f);
-        Cursor cursor = new Cursor(300.0f, 200.0f, arena, 10.0f);
-
+    public void testRotateCursor() {
         cursor.rotateLeft(60);
         cursor.move();
         cursor.rotateLeft(120);
