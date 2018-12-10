9fdb696532ff9f48139af3d5effc366b3cd66746
Paweł Gędłek <pawelgedlek@gmail.com>
2018-12-10 18:20:19 +0100

    Adding nested loop

--- src/main/java/pl/edu/agh/to2/kitkats/codinlearner/parser/CommandParser.java
+++ src/main/java/pl/edu/agh/to2/kitkats/codinlearner/parser/CommandParser.java
@@ -17,86 +17,74 @@ public class CommandParser {
         this.commandMap = commandMap;
     }
 
-    public List<List<Command>> parseCommand(String commandAsString){
+    public List<List<Command>> parseCommand(String commandAsString) {
+        System.out.println(commandAsString);
         List<List<Command>> commands = new ArrayList<>();
-        List<String> parts = Arrays.asList(commandAsString.split(" "));
+        List<String> parts = Arrays.asList(commandAsString.split("\\s"));
 
-        for(int i=0; i<parts.size(); i++){
+        for (int i = 0; i < parts.size(); i++) {
             String currentCommand = parts.get(i);
 
-            if(currentCommand.equals("repeat")){
+            if (currentCommand.equals("repeat")) {
                 List<Command> lineCommands = new ArrayList<>();
                 i++;
                 try {
                     Integer repeats = 0;
-                    if(i < parts.size()){ repeats = Integer.parseInt(parts.get(i)); i++; }
-                    String loopContent = commandAsString.substring(commandAsString.indexOf("[")+1,commandAsString.indexOf("]"));
-                    for(int r=0; r<repeats; r++) {
-                        commands.addAll(parseLineCommand(loopContent));
+                    if (i < parts.size()) {
+                        repeats = Integer.parseInt(parts.get(i));
+                        i++;
+                    }
+                    String loopContent = commandAsString.substring(commandAsString.indexOf("[") + 1, commandAsString.lastIndexOf("]"));
+                    for (int r = 0; r < repeats; r++) {
+                        commands.addAll(parseCommand(loopContent));
                     }
                     i = commandAsString.indexOf("]");
-                } catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     lineCommands.add(Command.WRONG);
                     commands.add(lineCommands);
                     //return if we have loop
                     return commands;
                 }
+            } else {
+                List<Command> lineCommands = new ArrayList<>();
+                if (commandMap.containsKey(parts.get(i))) {
+                    currentCommand = parts.get(i);
+                    Command comm;
+                    //check next string
+                    if (i + 1 < parts.size() && !commandMap.containsKey(parts.get(i + 1))) {
+                        comm = parseComplexCommand(currentCommand, parts.get(i + 1));
+                        lineCommands.add(comm);
+                        commands.add(lineCommands);
+                        i++;
+                    } else {
+                        comm = parseSimpleCommand(currentCommand);
+                        if (currentCommand.equals("go")) comm.setDefaultsValues();
+                        lineCommands.add(comm);
+                        commands.add(lineCommands);
+                    }
+                } else {
+                    lineCommands.add(Command.WRONG);
+                    commands.add(lineCommands);
+                }
             }
-            else{
-                commands.addAll(parseLineCommand(commandAsString));
-                return commands;
-            }
-
         }
         //return if empty
         return commands;
     }
 
-    public Command parseSimpleCommand(String currentCommand){
+    public Command parseSimpleCommand(String currentCommand) {
         return commandMap.getOrDefault(currentCommand, Command.WRONG);
     }
 
-    public Command parseComplexCommand(String currentCommand, String nextCommand){
+    public Command parseComplexCommand(String currentCommand, String nextCommand) {
         Command comm = commandMap.getOrDefault(currentCommand, Command.WRONG);
         try {
             Integer arg = Integer.parseInt(nextCommand);
             comm.setValue(arg);
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return Command.WRONG;
         }
 
         return comm;
     }
-
-    public List<List<Command>> parseLineCommand(String commandAsString){
-        List<List<Command>> commands = new ArrayList<>();
-        List<String> parts = Arrays.asList(commandAsString.split(" "));
-
-
-        for(int i=0; i<parts.size(); i++){
-            List<Command> lineCommands = new ArrayList<>();
-            if(commandMap.containsKey(parts.get(i))){
-                String currentCommand = parts.get(i);
-                Command comm;
-                //check next string
-                if(i+1 < parts.size() && !commandMap.containsKey(parts.get(i+1))){
-                    comm = parseComplexCommand(currentCommand, parts.get(i+1));
-                    lineCommands.add(comm);
-                    commands.add(lineCommands);
-                    i++;
-                } else {
-                    comm = parseSimpleCommand(currentCommand);
-                    if(currentCommand.equals("go")) comm.setDefaultsValues();
-                    lineCommands.add(comm);
-                    commands.add(lineCommands);
-                }
-            } else {
-                lineCommands.add(Command.WRONG);
-                commands.add(lineCommands);
-            }
-        }
-
-        return commands;
-    }
-
 }
