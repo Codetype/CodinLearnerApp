14bae22e9a326865075572b76dd344348f1c0fb9
Paweł Gędłek <pawelgedlek@gmail.com>
2018-12-10 14:57:26 +0100

    Changing Command Enum properties

--- src/main/java/pl/edu/agh/to2/kitkats/codinlearner/model/Command.java
+++ src/main/java/pl/edu/agh/to2/kitkats/codinlearner/model/Command.java
@@ -1,8 +1,20 @@
 package pl.edu.agh.to2.kitkats.codinlearner.model;
 
 public enum Command {
+    RIGHT(90), LEFT(90), FORWARD(1), BACK(1), WRONG(0), EMPTY(0), REPEAT(0);
 
-    RIGHT, LEFT, FORWARD, BACK, WRONG, EMPTY;
+    private int value;
+    Command(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 
     public Command oppositeCommand(){
         switch (this){
