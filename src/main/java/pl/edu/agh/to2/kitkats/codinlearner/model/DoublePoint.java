60754ca9301707ee80b93c00c6c7c161bb19a990
jakub <jstarzyk98@gmail.com>
2018-12-10 18:24:21 +0100

    Change DoublePoint fields

--- src/main/java/pl/edu/agh/to2/kitkats/codinlearner/model/DoublePoint.java
+++ src/main/java/pl/edu/agh/to2/kitkats/codinlearner/model/DoublePoint.java
@@ -2,11 +2,27 @@ package pl.edu.agh.to2.kitkats.codinlearner.model;
 
 public class DoublePoint {
 
-    public final double x;
-    public final double y;
+    public double x;
+    public double y;
 
     public DoublePoint(double x, double y) {
         this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
         this.y = y;
     }
 }
