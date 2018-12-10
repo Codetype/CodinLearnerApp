c5367d6261dcd9dc195416a81a4f6732310cb741
jakub <jstarzyk98@gmail.com>
2018-12-10 19:20:46 +0100

    Improve LevelProvider

--- src/main/java/pl/edu/agh/to2/kitkats/codinlearner/level/LevelProvider.java
+++ src/main/java/pl/edu/agh/to2/kitkats/codinlearner/level/LevelProvider.java
@@ -1,10 +1,9 @@
 package pl.edu.agh.to2.kitkats.codinlearner.level;
 
+import com.vividsolutions.jts.algorithm.Angle;
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.math.Vector2D;
-import javafx.util.Pair;
 import pl.edu.agh.to2.kitkats.codinlearner.model.Command;
-import pl.edu.agh.to2.kitkats.codinlearner.model.Cursor;
-import pl.edu.agh.to2.kitkats.codinlearner.model.DoublePoint;
 import pl.edu.agh.to2.kitkats.codinlearner.model.MoveGraph;
 
 import java.util.ArrayList;
@@ -12,70 +11,52 @@ import java.util.List;
 
 public class LevelProvider {
 
-    private static List<Level> levels = new ArrayList<>();
-    private float moveStep;
-//    private Cursor cursor;
-//    private Vector2D startVector;
+    private List<Level> levels;
+    private double moveStep;
 
     public LevelProvider(float moveStep) {
         this.moveStep = moveStep;
+        this.levels = new ArrayList<>();
     }
 
-    private void tmp(Vector2D moveVector) {
-
-    }
-
-//    private
-
-    private Level newLevel(List<Command> commands, String description) {
+    private void newLevel(List<Command> commands, String description) {
         MoveGraph graph = new MoveGraph();
-//        double moveStep = startVector.length();
-//        Vector2D vector = new Vector2D();
-//        Vector2D vector = startVector;
-        DoublePoint from = new DoublePoint(0.0, 0.0);
-        DoublePoint to = new DoublePoint(0.0, 0.0);
+        Coordinate from = new Coordinate(0.0, 0.0);
+        Coordinate to = new Coordinate(0.0, 0.0);
+        Vector2D moveVector = new Vector2D(moveStep, 0);
 
         for (Command command : commands) {
             int value = command.getValue();
-            switch (command) {
-                case FORWARD:
-                    break;
-                case BACK:
-                    break;
-                case LEFT:
-                    break;
-                case RIGHT:
-                    break;
+
+            if (command == Command.LEFT) {
+                moveVector = moveVector.rotate(Angle.toRadians(-value));
+            } else if (command == Command.RIGHT) {
+                moveVector = moveVector.rotate(Angle.toRadians(value));
+            } else {
+                if (command != Command.FORWARD && command != Command.BACK) {
+                    continue;
+                }
+                for (int i = 0; i < value; i++) {
+                    switch (command) {
+                        case FORWARD:
+                            to.x += moveVector.getX();
+                            to.y += moveVector.getY();
+                            break;
+                        case BACK:
+                            to.x -= moveVector.getX();
+                            to.y -= moveVector.getY();
+                            break;
+                    }
+                    graph.addVertex(from.x, from.y, to.x, to.y);
+                    from.setCoordinate(to);
+                }
             }
         }
 
-        Level result = new Level(graph, description);
-        return result;
+        levels.add(new Level(graph, description));
     }
 
     private void initializeLevels() {
-
     }
 
-    public void getLevels() {
-
-    }
-
-
-    public void initializeLevels2() {
-        // level 1
-        int commandNumber = 2;
-//        float lineLength = commandNumber * arena.getCursor().getMoveStep();
-        MoveGraph task1 = new MoveGraph();
-        task1.addVertex(0.0, 0.0, 50.0, 0.0);
-        task1.addVertex(50.0, 0.0, 100.0, 0.0);
-        task1.addVertex(100.0, 0.0, 100.0, 50.0);
-        task1.addVertex(100.0, 50.0, 100.0, 100.0);
-        task1.addVertex(100.0, 100.0, 50.0, 100.0);
-        task1.addVertex(50.0, 100.0, 0.0, 100.0);
-        task1.addVertex(0.0, 100.0, 0.0, 50.0);
-        task1.addVertex(0.0, 50.0, 0.0, 0.0);
-        Level l1 = new Level(task1, "Draw a line (length: " + commandNumber + ")");
-//        levelManager.addLevel(l1);
-    }
 }
