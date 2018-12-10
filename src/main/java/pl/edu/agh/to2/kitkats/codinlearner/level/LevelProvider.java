2cd250f15aa1a95441208e9f10ac5740fd9751b3
jakub <jstarzyk98@gmail.com>
2018-12-10 19:55:32 +0100

    Add 5 initial Levels

--- src/main/java/pl/edu/agh/to2/kitkats/codinlearner/level/LevelProvider.java
+++ src/main/java/pl/edu/agh/to2/kitkats/codinlearner/level/LevelProvider.java
@@ -3,10 +3,13 @@ package pl.edu.agh.to2.kitkats.codinlearner.level;
 import com.vividsolutions.jts.algorithm.Angle;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.math.Vector2D;
+import javafx.util.Pair;
 import pl.edu.agh.to2.kitkats.codinlearner.model.Command;
 import pl.edu.agh.to2.kitkats.codinlearner.model.MoveGraph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LevelProvider {
@@ -19,14 +22,15 @@ public class LevelProvider {
         this.levels = new ArrayList<>();
     }
 
-    private void newLevel(List<Command> commands, String description) {
+    private void newLevel(List<Pair<Command, Integer>> commands, String description) {
         MoveGraph graph = new MoveGraph();
         Coordinate from = new Coordinate(0.0, 0.0);
         Coordinate to = new Coordinate(0.0, 0.0);
         Vector2D moveVector = new Vector2D(moveStep, 0);
 
-        for (Command command : commands) {
-            int value = command.getValue();
+        for (Pair<Command, Integer> pair : commands) {
+            Command command = pair.getKey();
+            int value = pair.getValue();
 
             if (command == Command.LEFT) {
                 moveVector = moveVector.rotate(Angle.toRadians(-value));
@@ -57,6 +61,67 @@ public class LevelProvider {
     }
 
     private void initializeLevels() {
+        newLevel(
+                Collections.singletonList(new Pair<>(Command.FORWARD, 2)),
+                "Draw a line (length: 2)"
+        );
+        newLevel(
+                Arrays.asList(
+                        new Pair<>(Command.FORWARD, 2),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 2),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90)
+                ),
+                "Draw a rectangle (width: 2, height: 1)"
+        );
+        newLevel(
+                Arrays.asList(
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 90)
+                ),
+                "Draw a square (size: 1)"
+        );
+        newLevel(
+                Arrays.asList(
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 120),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 120),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.LEFT, 120)
+                ),
+                "Draw an equilateral triangle (size: 1)"
+        );
+        newLevel(
+                Arrays.asList(
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.BACK, 1),
+                        new Pair<>(Command.RIGHT, 72),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.BACK, 1),
+                        new Pair<>(Command.RIGHT, 72),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.BACK, 1),
+                        new Pair<>(Command.RIGHT, 72),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.BACK, 1),
+                        new Pair<>(Command.RIGHT, 72),
+                        new Pair<>(Command.FORWARD, 1),
+                        new Pair<>(Command.BACK, 1),
+                        new Pair<>(Command.RIGHT, 72)
+                ),
+                "Draw an asterisk (size: 1, arms: 5)"
+        );
     }
 
 }
